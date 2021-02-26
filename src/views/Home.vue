172ac696ed22fb33e34e491bde20b37fffc20bf0
4l1c30zz <4l1c30zz@gmail.com>
2021-02-26 02:19:30 +0200

    hero extended

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="flex-center container">
-    <div class="column is-half">
-      <h1 class="is-center">lallala titleText</h1>
+  <div class="flex-center container hero">
+    <div class="column is-full">
+      <h1 class="is-center is-uppercase neon-sign">Hey There</h1>
     </div>
-    <div class="column is-half">
+    <div class="column is-full hero-paragraph">
       <p class="is-left">
-        My name is Alice. I’m a Tel-Aviv based self-taught creative developer
-        that loves bring concepts to life and explore new things. Favorite
-        stuff: sketching, good music, strong coffee, and fresh challenges
+        My name is Alice, I’m a Tel-Aviv based self-taught creative developer.
       </p>
+      <p class="is-left">
+        Melomaniac with formal music education. Always searching for the best
+        tunes around
+      </p>
+      <p class="is-left">
+        Ink/paper waster(love sketching, lettering,doodling etc), and casual
+        games player=)
+      </p>
+    </div>
+  </div>
+  <div class="flex-center container hero">
+    <div class="column is-full">
+      <h2 class="is-uppercase">skills</h2>
+      <div>
+        <p class="is-left">
+       <b> Frontend:</b> [ Common: <br/>[ HTML, CSS, SCSS, ], JS: <br/>[ jQuery, Handlebars.js,
+        Anime.js, Axios, Apollo ,Cypress ] Frameworks: <br/>[ Vue.js, Vuex] ],
+        </p>
+        <p class="is-left">
+       <b> Backend: </b> [ PHP, mySQL, Smarty, Strapi, ],<br/> Frameworks:<br/>[ WordPress ],
+        </p>
+        <p class="is-left">
+        <b>Design: </b>[ Figma, Adobe XD, Adobe Photoshop, Adobe Illustrator ],
+        </p>
+        <p class="is-left">
+        <b> Misc: </b>[Ubuntu, bash, Git ]
+        </p>
+      </div>
     </div>
   </div>
 </template>
