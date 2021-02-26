3db9e195899a4f31679adc80205fbfe58ec08acc
4l1c30zz <4l1c30zz@gmail.com>
2021-02-26 11:09:30 +0200

    basic posts grid added

---
+++ src/components/postsGrid.vue
@@ -0,0 +1,38 @@
+<template>
+  <article class="post column is-three" v-for="post in posts" :ref="postsRefs" v-bind:key="post">
+    <div class="post-content">
+    <span>
+      {{ post.category.name }}
+    </span>
+    <img :src="env + post.image.url" />
+    <p>{{ breakSentence(post.tech)}}</p>
+    <h3>{{ post.title }}</h3>
+    </div>
+    <div class="post-toolbar">
+      <a v-if="post.live" :href="post.live">live</a>
+      <a v-if="post.git" :href="post.git">git</a>
+    </div>
+  </article>
+</template>
+<script>
+export default {
+  name: 'postsGrid',
+  props: ['posts'],
+  data() {
+    return {
+      postsRefs: [],
+      env: process.env.VUE_APP_API_URI,
+    };
+  },
+  methods: {
+    setPostRef(elem) {
+      if (elem) {
+        this.postsRefs.push(elem);
+      }
+    },
+    breakSentence(value) {
+      return value ? value.split(' ').filter(Boolean) : '';
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-center container hero">
+  <div class="flex flex-center container is-fluid hero">
     <div class="column is-full flex flex-start">
       <h1 class="is-center is-uppercase neon-sign">Hey There</h1>
     </div>
@@ -20,24 +20,43 @@
   <div class="flex flex-center container">
     <div class="column is-full flex flex-start">
       <h2 class="is-uppercase is-full"><b>| </b>skills</h2>
-        <p class="is-left is-full">
-       <b> Frontend:</b> [ Common: <br/>[ HTML, CSS, SCSS, ], JS: <br/>[ jQuery, Handlebars.js,
-        Anime.js, Axios, Apollo ,Cypress ] Frameworks: <br/>[ Vue.js, Vuex] ],
-        </p>
-        <p class="is-left">
-       <b> Backend: </b> [ PHP, mySQL, Smarty, Strapi, ],<br/> Frameworks:<br/>[ WordPress ],
-        </p>
-        <p class="is-left">
+      <p class="is-left is-full">
+        <b> Frontend:</b> [ Common: <br />[ HTML, CSS, SCSS, ], JS: <br />[
+        jQuery, Handlebars.js, Anime.js, Axios, Apollo ,Cypress ] Frameworks:
+        <br />[ Vue.js, Vuex] ],
+      </p>
+      <p class="is-left">
+        <b> Backend: </b> [ PHP, mySQL, Smarty, Strapi, ],<br />
+        Frameworks:<br />[ WordPress ],
+      </p>
+      <p class="is-left">
         <b>Design: </b>[ Figma, Adobe XD, Adobe Photoshop, Adobe Illustrator ],
-        </p>
-        <p class="is-left">
-        <b> Misc: </b>[Ubuntu, bash, Git ]
-        </p>
-      </div>
+      </p>
+      <p class="is-left"><b> Misc: </b>[Ubuntu, bash, Git ]</p>
     </div>
+  </div>
+  <div class="flex flex-center container">
+    <div class="column is-full flex flex-start">
+      <h2 class="is-uppercase is-full">Development</h2>
+      <postsGrid
+        :posts="this.posts"
+      />
+    </div>
+  </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
+import postsGrid from '@/components/postsGrid.vue';
+
 export default {
   name: 'Home',
+  components: {
+    postsGrid,
+  },
+  computed: {
+    ...mapGetters({
+      posts: 'getBlog',
+    }),
+  },
 };
 </script>
