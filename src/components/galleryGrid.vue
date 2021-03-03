eeb156109e0595ac7a88fecca7db2835d1f6bf15
4l1c30zz <4l1c30zz@gmail.com>
2021-03-03 12:10:38 +0200

    basic gallery movements finished

--- src/components/galleryGrid.vue
+++ src/components/galleryGrid.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="column is-three gallery"
+  <section class="container flex flex-center gallery_wrap">
+    <div class="close" @click="closeGallery">X</div>
+      <div class="arr arr-left" @click="goLeft">LEFT</div>
+      <div class="arr arr-right" @click="goright">RIGHT</div>
+    <div
+      class="column is-three gallery"
       v-for="galleryItem in galleryItems"
       :ref="setGalleryRefs"
       v-bind:key="galleryItem"
-     >
-     <img class="gallery-image" v-if="galleryItem.url" :src="env + galleryItem.url"/>
-     <h4 class="gallery-heading" v-if="galleryItem.caption" >{{galleryItem.caption}}</h4>
-  </div>
+      @click="galleryItemSelected"
+    >
+      <img
+        class="gallery-image"
+        v-if="galleryItem.url"
+        :src="env + galleryItem.url"
+      />
+      <h4 class="gallery-heading" v-if="galleryItem.caption">
+        {{ galleryItem.caption }}
+      </h4>
+    </div>
+  </section>
 </template>
 <script>
 export default {
@@ -21,6 +34,46 @@ export default {
   methods: {
     setGalleryRefs(item) {
       return item ? this.galleryItemRefs.push(item) : false;
+    },
+    galleryItemSelected(e) {
+      const targ = e.currentTarget;
+      const targSiblings = [...targ.parentNode.children].filter(
+        (sibling) => sibling !== targ,
+      );
+      targ.classList.add('a');
+      targSiblings.forEach((element) => {
+        if (element.classList.contains('a')) {
+          element.classList.remove('a');
+        }
+      });
+    },
+    goLeft() {
+      const activeItem = document.querySelector('.gallery.a');
+      if (activeItem) {
+        const prev = activeItem.previousSibling;
+        if (prev) {
+          prev.classList.add('a');
+          activeItem.classList.remove('a');
+        }
+      }
+    },
+    goright() {
+      const activeItem = document.querySelector('.gallery.a');
+      if (activeItem) {
+        const next = activeItem.nextSibling;
+        if (next) {
+          next.classList.add('a');
+          activeItem.classList.remove('a');
+        }
+      }
+    },
+    closeGallery() {
+      const activeItems = document.querySelectorAll('.gallery.a');
+      if (activeItems) {
+        activeItems.forEach((elem) => {
+          elem.classList.remove('a');
+        });
+      }
     },
   },
 };
