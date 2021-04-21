c7a148b0fa43ef21bb614dcaaec100aa330a85fa
4l1c30zz <4l1c30zz@gmail.com>
2021-04-21 22:53:48 +0300

    insta added

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,26 +1,34 @@
 <template>
   <footer id="contact" class="footer flex flex-center">
     <div class="footer-wrap flex flex-center">
-    <div class="column flex-start is-full padless is-center">
-      <Logo />
-      <a href="mailto:4l1c30zz@gmail.com"
-      class="is-bold is-uppercase is-underlined mail-link"
-      >
-      4l1c30zz@gmail.com
-      </a>
-    </div>
-    <div class="footer-toolbar padless column is-full flex flex-between">
-      <a href="https://github.com/4l1c30zz/" target="_blank" class="button">
-        GitHub
-      </a>
-      <a
-        href="https://www.behance.net/4l1c30zz/"
-        target="_blank"
-        class="button"
-      >
-        Behance
-      </a>
-    </div>
+      <div class="column flex-start is-full padless is-center">
+        <Logo />
+        <a
+          href="mailto:4l1c30zz@gmail.com"
+          class="is-bold is-uppercase is-underlined mail-link"
+        >
+          4l1c30zz@gmail.com
+        </a>
+      </div>
+      <div class="footer-toolbar padless column is-full flex flex-between">
+        <a href="https://github.com/4l1c30zz/" target="_blank" class="button">
+          GitHub
+        </a>
+        <a
+          href="https://www.behance.net/4l1c30zz/"
+          target="_blank"
+          class="button"
+        >
+          Behance
+        </a>
+        <a
+          href="https://www.instagram.com/4l1c30zz/"
+          target="_blank"
+          class="button"
+        >
+          Instagram
+        </a>
+      </div>
     </div>
   </footer>
 </template>
